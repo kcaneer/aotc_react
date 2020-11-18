3e415db5a8ec726e15fda3654a074fd8b298dcb3
kcaneer <kcaneer@gmail.com>
2020-11-18 11:41:32 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap'
+import Header from './components/Header'
+import './theme.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container text-primary bg-secondary pb-4">
+      <Header/>
+      <h1>Welcome to GoodListens</h1>
+      <h4>Your podcast place.</h4>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import '../theme.css'
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary mb-3">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a className="navbar-brand text-secondary" href="#">
+          GoodListens{" "}
+        </a>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link text-secondary" href="#">
+              Want To Listen
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-secondary" href="#">
+              Listened
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-secondary" href="#">
+              Another tab
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Find a podcast here"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-secondary my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
