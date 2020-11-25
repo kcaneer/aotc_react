ff37362a76c22e258dca3eabb84151098cd7f33e
kcaneer <kcaneer@gmail.com>
2020-11-25 14:29:14 -0500

    implemented axios helper

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,38 +1,25 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosHelper } from '../Utilities/axiosHelper';
 import { useHistory } from "react-router-dom";
 
 export default function Login(){
   const[email, setEmail] = useState('') 
   const[password, setPassword] = useState('') 
   const history = useHistory();
+  
   const handleClick = event => {
     const headers = {
       'Accept': "application/json",
       "Content-Type": "application/json; charset=utf-8",
     };
-    axios({
-      url: "http://127.0.0.1:8000/v1/oauth/token",
-      method: "post",
-      data: {
+    axiosHelper('post', 'http://127.0.0.1:8000/v1/oauth/token', {
         grant_type: "password",
         client_id: "6",
         client_secret: "2dtobHf0U4BXyhnzUeZz7b6WMUf2tp0SxAJFbPkd",
         password,
         username: email,
         scope: "",
-      },
-      headers,
-    })
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          history.push("/dashboard");
-        }
-      })
-      .catch((error) => {
-        console.log("login error", error);
-      });
+      }, headers, history) 
   }
     return (
       <div className="p-5 my-auto">
