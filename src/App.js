08fbf8854231a5923603f3ff059b0a64b61e6d72
kcaneer <kcaneer@gmail.com>
2020-12-01 07:56:59 -0500

    getting user info back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import 'bootstrap'
-import './theme.css'
+import React, {useState} from "react";
+import "bootstrap";
+import "./theme.css";
 import "./App.css";
-import Want from './components/Want'
-import Listened from './components/Listened'
-import Public from './components/Public'
-import Dashboard from './components/Dashboard'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import Want from "./components/Want";
+import Listened from "./components/Listened";
+import Public from "./components/Public";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AppProvider } from "./Utilities/AppContext";
 function App() {
+  const [bearer, setBearer] = useState("");
+  const [name, setName] = useState("");
+  let initialContext = {bearer, setBearer, name, setName};
   return (
-    <div className="App container text-primary bg-secondary pb-4">
-      <Router>
-        <Switch>
-        <Route exact path={"/"} component={Public} />
-        <Route exact path={"/dashboard"} component={Dashboard} />
-        <Route exact path={"/dashboard/want"} component={Want} />
-        <Route exact path={"/dashboard/listened"} component={Listened} />
-        </Switch>
-      </Router>
-    </div>
+    <AppProvider value={initialContext}>
+      <div className="App container text-primary bg-secondary pb-4">
+        <Router>
+          <Switch>
+            <Route exact path={"/"} component={Public} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+            <Route exact path={"/dashboard/want"} component={Want} />
+            <Route exact path={"/dashboard/listened"} component={Listened} />
+            <Route exact path={"/dashboard/logout"} component={Public}/>
+          </Switch>
+        </Router>
+      </div>
+    </AppProvider>
   );
 }
 
--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,50 +1,64 @@
-import React, { useState } from "react";
-import { axiosHelper } from '../Utilities/axiosHelper';
+import React, { useState, useContext } from "react";
+import { axiosHelper } from "../../Utilities/axiosHelper";
 import { useHistory } from "react-router-dom";
+import AppContext from '../../Utilities/AppContext'
 
-export default function Login(){
-  const[email, setEmail] = useState('') 
-  const[password, setPassword] = useState('') 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
-  
-  const handleClick = event => {
+  const {setBearer} = useContext(AppContext);
+
+  function receivedInfo(data) {
+    setBearer(data.access_token);
+  }
+
+
+  const handleClick = (event) => {
     const headers = {
-      'Accept': "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json; charset=utf-8",
     };
-    axiosHelper('post', 'http://127.0.0.1:8000/v1/oauth/token', {
+    axiosHelper({
+      method: "post",
+      url: "http://127.0.0.1:8000/v1/oauth/token",
+      data: {
         grant_type: "password",
         client_id: "6",
         client_secret: "2dtobHf0U4BXyhnzUeZz7b6WMUf2tp0SxAJFbPkd",
         password,
         username: email,
         scope: "",
-      }, headers, history) 
-  }
-    return (
-      <div className="p-5 my-auto">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button
-            className="bg-primary text-secondary rounded ml-1 border border-primary"
-            onClick={handleClick}
-          >
-            Login
-          </button>
-      </div>
-    );
-  }
+      },
+      headers,
+      history,
+      functionToRun: receivedInfo,
+    });
+  };
+  return (
+    <div className="p-5 my-auto">
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button
+        className="bg-primary text-secondary rounded ml-1 border border-primary"
+        onClick={handleClick}
+      >
+        Login
+      </button>
+    </div>
+  );
+}
